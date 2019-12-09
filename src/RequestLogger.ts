7d6cfd49695989ae81b3c41bad8b55b924b7192b
Marcio Paixao Dantas <marciopd@gmail.com>
2019-12-09 13:25:56 +0100

    Avoiding issues resolving 'this'.

--- src/RequestLogger.ts
+++ src/RequestLogger.ts
@@ -2,18 +2,19 @@ import { JsonLogger } from './JsonLogger';
 import { LoggerFactory } from './LoggerFactory';
 
 export class RequestLogger {
-  private readonly logger: JsonLogger = LoggerFactory.createLogger(RequestLogger.name);
+  private static readonly LOGGER: JsonLogger = LoggerFactory.createLogger(RequestLogger.name);
 
   public logExpressRequest(req, res, next): void {
     const requestPath = req && req.route && req.route.path;
     if (!requestPath) {
-      this.logger.warn('No request path defined.');
+      RequestLogger.LOGGER.warn('No request path defined.');
       next();
       return;
     }
 
-    this.logger.info(`Before request '${requestPath}'`);
+    RequestLogger.LOGGER.info(`Before request '${requestPath}'`);
     next();
-    this.logger.info(`After request '${requestPath}'`);
+    RequestLogger.LOGGER.info(`After request '${requestPath}'`);
+    return;
   }
 }
