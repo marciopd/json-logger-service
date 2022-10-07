caf857cf12a9f01ce3032dd0dd7ef42e9c827bc9
mush-8room <green_pea88@likelion.net>
2022-10-07 15:54:01 +0900

    :art: UPDATE: change applying stream

--- src/LoggerFactory.ts
+++ src/LoggerFactory.ts
@@ -1,36 +1,33 @@
-import * as Logger from 'bunyan';
-import * as PrettyStream from 'bunyan-prettystream';
-import {JsonLogger} from './JsonLogger';
-import {CustomContextBuilderInterface} from './CustomContextBuilder.interface';
+import * as Logger from "bunyan";
+import { JsonLogger } from "./JsonLogger";
+import { CustomContextBuilderInterface } from "./CustomContextBuilder.interface";
 
 export class LoggerFactory {
-    public static defaultCustomContextBuilder: CustomContextBuilderInterface;
+  public static defaultCustomContextBuilder: CustomContextBuilderInterface;
 
-    // tslint:disable-next-line:typedef
-    public static setDefaultLogCustomContextBuilder(
-        defaultCustomContextBuilder: CustomContextBuilderInterface) {
-        LoggerFactory.defaultCustomContextBuilder = defaultCustomContextBuilder;
-    }
+  // tslint:disable-next-line:typedef
+  public static setDefaultLogCustomContextBuilder(
+    defaultCustomContextBuilder: CustomContextBuilderInterface
+  ) {
+    LoggerFactory.defaultCustomContextBuilder = defaultCustomContextBuilder;
+  }
 
-    public static createLogger(
-        name: string,
-        customContextBuilder?: CustomContextBuilderInterface): JsonLogger {
+  public static createLogger(
+    name: string,
+    customContextBuilder?: CustomContextBuilderInterface
+  ): JsonLogger {
+    let stream = undefined;
+    stream.pipe(process.stdout);
 
-        let stream = undefined;
-        if (process.env.LOGGER_PRETTY_PRINT === 'true') {
-            stream = new PrettyStream();
-            stream.pipe(process.stdout);
-        }
-
-        const bunyanLogger = Logger.createLogger({
-                                                     name,
-                                                     serializers: {
-                                                         error: Logger.stdSerializers.err,
-                                                     },
-                                                     env: process.env.LOGGER_ENV,
-                                                     level: process.env.LOGGER_LEVEL || 'info',
-                                                     stream: stream,
-                                                 });
-        return new JsonLogger(bunyanLogger, customContextBuilder);
-    }
+    const bunyanLogger = Logger.createLogger({
+      name,
+      serializers: {
+        error: Logger.stdSerializers.err,
+      },
+      env: process.env.LOGGER_ENV,
+      level: process.env.LOGGER_LEVEL || "info",
+      stream: stream,
+    });
+    return new JsonLogger(bunyanLogger, customContextBuilder);
+  }
 }
